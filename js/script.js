63f2fc956acd07e3b2cf0eb66330b6b07e4c430e
JohnSmillie <jsmillie23@gmail.com>
2023-07-31 09:38:47 -0500

    landing page design complete. Needs active links.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
 
 function aboutMe(activity){
   var tennis = "Add ten years to your life - start playing tennis. The game of tennis has always been a big part of my family. Now, I too am loving the game.";
-  var music = "I love music.";
-  var petey = "I love my cat Petey.";
-  var lifestyle = "Mind, body, and soul.";
+  var music = "I've been playing music for most of my life, and for some years music was my career. I attended Roosevelt University where I studied jazz. ";
+  var petey = "I have a deep love of animals, and I lived with cats for most of my life. Now, I have a white and orange short haired cat named Petey. He likes to play fetch and cuddle. I love my cat Petey.";
+  var lifestyle = "Mind, body, and soul. I adhere to a pescatarian diet for health and ethical reasons. I seek to reduce the unsustainable and unethical food sources in my life.";
 
   var buttons = document.getElementsByClassName("about-me-items");
   for( var i = 0; i < buttons.length; i++){
